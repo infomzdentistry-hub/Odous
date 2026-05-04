abb739ff500d36f8acd2b3f87ae32f306ba9ccda
Ali Khan <info.mzdentistry@.com>
2026-05-04 21:22:12 +0500

    Fix appointment forms - proper date and time binding

--- Odous/Models/Appointment.cs
+++ Odous/Models/Appointment.cs
@@ -5,12 +5,13 @@ namespace Odous.Models
     public class Appointment
     {
         public int Id { get; set; }
-        public int AppointmentMR { get; set; }   // Unique appointment number
+        public int AppointmentMR { get; set; }
         public string PatientName { get; set; } = "";
         public DateTime AppointmentDate { get; set; } = DateTime.Now;
-        public TimeSpan StartTime { get; set; } = new TimeSpan(9, 0, 0);
-        public TimeSpan EndTime { get; set; } = new TimeSpan(10, 0, 0);
+        public TimeOnly StartTime { get; set; } = new TimeOnly(9, 0);
+        public TimeOnly EndTime { get; set; } = new TimeOnly(10, 0);
         public string Reason { get; set; } = "";
+        public string ProcedureNotes { get; set; } = "";
         public string ServiceType { get; set; } = "";
         public string Dentist { get; set; } = "";
         public string Status { get; set; } = "Scheduled";
