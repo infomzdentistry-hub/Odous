d488bcfa97dfa5560b15a5565800467bf7e7b220
Ali Khan <info.mzdentistry@.com>
2026-05-04 17:00:47 +0500

    Fix AntiForgery token with persistent storage

--- Odous/Program.cs
+++ Odous/Program.cs
@@ -1,19 +1,25 @@
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.EntityFrameworkCore;
 using Odous.Components;
 using Odous.Data;
 using Odous.Services;
-using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// ADD THIS LINE FOR RENDER
+// Add port binding for Render
 builder.WebHost.UseUrls("http://*:8080");
+
+// Configure Data Protection to use a persistent volume
+builder.Services.AddDataProtection()
+    .PersistKeysToFileSystem(new DirectoryInfo("/var/data/keys"))
+    .SetApplicationName("OdousDental");
 
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 // Register Database Context
 builder.Services.AddDbContextFactory<ApplicationDbContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection") ?? "Data Source=/var/data/odous.db"));
 
 // Register Services with Database
 builder.Services.AddScoped<PatientService>();
